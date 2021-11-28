a09645e59f03b108bc618982ebac52a0c4655db0
jashan <jashandhiman.hulk@gmail.com>
2021-11-28 18:11:38 +0530

    add searching

--- notes.js
+++ notes.js
@@ -1,13 +1,27 @@
 var container2 = document.getElementsByClassName("container2")[0];
 var container3 = document.getElementsByClassName("container3")[0];
-var save = document.getElementById("save");
-var share = document.getElementById("share");
-var trash = document.getElementById("trash");
 var create = document.getElementById("create");
+var save = document.getElementById("save");
+var remove = document.getElementById("remove");
+// var share = document.getElementById("share");
+// var trash = document.getElementById("trash");
 var index = 0;
 var random_margin = ["-5px", "1px", "5px", "10px", "7px"];
 var random_colors = ["#c2ff3d","#ff3de8","#3dc2ff","#04e022","#bc83e6","#ebb328"];
 var random_degree = ["rotate(3deg)", "rotate(1deg)", "rotate(-1deg)", "rotate(-3deg)", "rotate(-5deg)", "rotate(-8deg)"];
+
+function search(val){
+	var filter = val.toUpperCase();
+	var notes = document.getElementById("notes");
+	var topic = notes.getElementsByTagName("h2");
+	for(i=0;i<topic.length; i++){
+		a = topic[i].innerHTML.toUpperCase();
+		if(a.indexOf(filter) > -1){
+			topic[i].parentNode.style.display = "";
+		}
+		else{
+			topic[i].parentNode.style.display = "none";
+}}}
 
 create.addEventListener("click",function(){
 	createNote();
@@ -15,12 +29,15 @@ create.addEventListener("click",function(){
 save.addEventListener("click",function(){
 	saveNote()
 })
-share.addEventListener("click",function(){
-	createNote()
+remove.addEventListener("click",function(){
+	removeNote()
 })
-trash.addEventListener("click",function(){
-	trashNote()
-})
+// share.addEventListener("click",function(){
+// 	createNote()
+// })
+// trash.addEventListener("click",function(){
+// 	trashNote()
+// })
 
 function createNote() {
 	if(container3.style.display = "none"){
@@ -29,6 +46,11 @@ function createNote() {
 	else{
 		container3.style.display = "none";
 	}
+}
+function removeNote() {
+		container3.style.display = "none";
+		document.getElementById('topic').value = "";
+		document.getElementById('note-text').value = "";
 }
 
 function saveNote() {
@@ -39,30 +61,32 @@ function saveNote() {
 	var node1 = document.createElement("h2");
 	var node2 = document.createElement("textarea");
 	if(topic && text){
- 	node1.innerHTML = topic;
-	node2.innerHTML = text;
-	node2.className = "content";
-	node2.readOnly = true;
+		node0.className = "note";
+		node2.className = "content";
+	 	node1.innerHTML = topic;
+		node2.innerHTML = text;
+		node2.readOnly = true;
 
-	node0.appendChild(node1);
-	node0.appendChild(node2);
-	notes.appendChild(node0);
-	node0.setAttribute("style", `height:200px;width:200px;margin-top:10px;border-radius: 5px;padding: 20px;
-		overflow: hidden;cursor: pointer;margin:${random_margin[Math.floor(Math.random() * random_margin.length)]}; 
-		background-color:${random_colors[index++]}; transform:${random_degree[Math.floor(Math.random() * random_degree.length)]}`);
+		node0.appendChild(node1);
+		node0.appendChild(node2);
+		notes.appendChild(node0);
+		node0.setAttribute("style", `margin:${random_margin[Math.floor(Math.random() * random_margin.length)]}; 
+			background-color:${random_colors[index++]}; transform:${random_degree[Math.floor(Math.random() * random_degree.length)]}`);
 
-	container3.style.display = "none";
+		document.getElementById('topic').value = "";
+		document.getElementById('note-text').value = "";
+		container3.style.display = "none";
 	}
 	else{
 		alert("something is missing in note");
 	}
 }
-function shareNote() {
+// function shareNote() {
 	
-}
-function trashNote() {
+// }
+// function trashNote() {
 	
-}
+// }
 
 // function myFunction(div){
 // 	var a = document.getElementById("open-note");
@@ -76,9 +100,6 @@ function trashNote() {
 // 	// a.appendChild(div);
 // 	div.parentNode.style.display = "block";
 // 	// a.childNodes[0].childNodes[1].style.display = "block";
-// }
-// function cross(div){
-// 	location.reload();
 // }
 function openNav() {
 	document.getElementById("menu").style.width = "100%";
